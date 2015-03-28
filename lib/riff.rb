24310f9d0977e4f378f352786b79e4f7a05ad39a
Johan Walles <johan.walles@gmail.com>
2015-03-28 15:01:20 +0100

    Identify main parts of diff
    We're still not identifying added and removed lines.

--- lib/riff.rb
+++ lib/riff.rb
@@ -1,7 +1,34 @@
 # Feed Riff a number of lines output from some diff-like tool (diff,
 # diff3, git diff, ...) and it will highlight that output for you.
 class Riff
+  DIFF_HEADER = /^diff /
+  DIFF_HUNK_HEADER = /^@@ /
+
+  def initialize()
+    @state = :initial
+  end
+
   def handle_diff_line(line)
-    puts line
+    if @state == :initial
+      if line =~ DIFF_HEADER
+        @state = :diff_header
+      end
+    elsif @state == :diff_header
+      if line =~ DIFF_HUNK_HEADER
+        @state = :diff_hunk_header
+      end
+    elsif @state == :diff_hunk_header
+      @state = :diff_hunk
+    elsif @state == :diff_hunk
+      if line =~ DIFF_HUNK_HEADER
+        @state = :diff_hunk_header
+      elsif line =~ DIFF_HEADER
+        @state = :diff_header
+      end
+    else
+      fail "Unknown state: #{@state}"
+    end
+
+    puts "<#{@state}>#{line}"
   end
 end
