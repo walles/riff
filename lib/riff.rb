4731af583efa5ff5ab67b2399ba5a7b06c6c28ee
Johan Walles <johan.walles@gmail.com>
2015-03-28 15:16:41 +0100

    Refactor to fix code complexity issues

--- lib/riff.rb
+++ lib/riff.rb
@@ -3,31 +3,45 @@
 class Riff
   DIFF_HEADER = /^diff /
   DIFF_HUNK_HEADER = /^@@ /
+  ADDED = /^\+ /
+  REMOVED = /^- /
+  CONTEXT = /^ /
 
   def initialize()
     @state = :initial
   end
 
-  def handle_diff_line(line)
-    if @state == :initial
-      if line =~ DIFF_HEADER
-        @state = :diff_header
-      end
-    elsif @state == :diff_header
-      if line =~ DIFF_HUNK_HEADER
-        @state = :diff_hunk_header
-      end
-    elsif @state == :diff_hunk_header
-      @state = :diff_hunk
-    elsif @state == :diff_hunk
-      if line =~ DIFF_HUNK_HEADER
-        @state = :diff_hunk_header
-      elsif line =~ DIFF_HEADER
-        @state = :diff_header
-      end
-    else
-      fail "Unknown state: #{@state}"
+  def handle_initial_line(line)
+    if line =~ DIFF_HEADER
+      @state = :diff_header
     end
+  end
+
+  def handle_diff_header_line(line)
+    if line =~ DIFF_HUNK_HEADER
+      @state = :diff_hunk_header
+    end
+  end
+
+  def handle_diff_hunk_header_line(_line)
+    @state = :diff_hunk
+  end
+
+  def handle_diff_hunk_line(line)
+    case line
+    when DIFF_HUNK_HEADER
+      @state = :diff_hunk_header
+    when DIFF_HEADER
+      @state = :diff_header
+    end
+  end
+
+  def handle_diff_line(line)
+    method_name = "handle_#{@state}_line"
+    fail "Unknown state: <:#{@state}>" unless
+      self.respond_to? method_name
+
+    send(method_name, line)
 
     puts "<#{@state}>#{line}"
   end
