140df76a899ad256b3fa65d58b3ac9186cc4c79a
Johan Walles <johan.walles@gmail.com>
2015-04-21 07:36:08 +0200

    Pager: Make a less() method to page using less

--- lib/pager.rb
+++ lib/pager.rb
@@ -1,9 +1,16 @@
+# Add a pager() method that can send text to a pager.
+#
+# With "pager" referring to something like less or moar.
 module Pager
+  def less(text)
+    IO.popen('LESS=FRX less', 'w') { |pager| pager.print text }
+  end
+
   def page(text)
-    if $stdout.isatty
-      IO.popen('LESS=FRX less', 'w') { |pager| pager.print text }
-    else
+    if !$stdout.isatty
       print text
+    else
+      less(text)
     end
   end
 end
