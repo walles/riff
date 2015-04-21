5c344bb76ea5d3a8ef7a1256da6a78e7618aa792
Johan Walles <johan.walles@gmail.com>
2015-04-21 08:00:07 +0200

    Pager: Use $PAGER as a pager if available
    Or fall back to less.

--- lib/pager.rb
+++ lib/pager.rb
@@ -2,13 +2,41 @@
 #
 # With "pager" referring to something like less or moar.
 module Pager
+  # Checking for this variable before launching $PAGER should prevent
+  # us from fork bombing if somebody sets the PAGER environment
+  # variable to point to us.
+  DONT_USE_PAGER_VAR = '_RIFF_PREVENT_PAGING_LOOP'
+
+  def pipe_text_into_command(text, command)
+    env = {
+      DONT_USE_PAGER_VAR => '1'
+    }
+
+    # Set LESS=FRX unless $LESS already has a value
+    env['LESS'] = 'FRX' unless ENV['LESS']
+
+    IO.popen(env, command, 'w') { |pager| pager.print text }
+  end
+
   def less(text)
-    IO.popen('LESS=FRX less', 'w') { |pager| pager.print text }
+    pipe_text_into_command(text, 'less')
+  end
+
+  # If $DONT_USE_PAGER_VAR is set, we shouldn't use $PAGER
+  def dont_use_pager?
+    return true if ENV[DONT_USE_PAGER_VAR]
+    return true if ENV['PAGER'].nil?
+    return true if ENV['PAGER'].empty?
+    return false
   end
 
   def page(text)
     if !$stdout.isatty
       print text
+    elsif dont_use_pager?
+      less(text)
+    elsif ENV['PAGER']
+      pipe_text_into_command(text, ENV['PAGER'])
     else
       less(text)
     end
