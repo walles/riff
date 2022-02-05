7fbec9c86e95003bd15ddeea5394218af9f1b1f1
Johan Walles <johan.walles@gmail.com>
2022-02-05 15:23:53 +0100

    Bump to a newer macOS SDK

--- release.sh
+++ release.sh
@@ -12,7 +12,7 @@
 
 set -eu -o pipefail
 
-CROSSBUILD_MACOS_SDK="macosx12.0"
+CROSSBUILD_MACOS_SDK="macosx12.1"
 
 # If this fails, try "xcodebuild -showsdks" to find one that exists
 if ! xcrun -sdk $CROSSBUILD_MACOS_SDK --show-sdk-path >/dev/null; then
