0952855ae331d04c64e116a739dbaca27370b0d5
Johan Walles <johan.walles@gmail.com>
2023-09-23 16:42:26 +0200

    Store ranges instead

--- src/hunk_header_parser.rs
+++ src/hunk_header_parser.rs
@@ -1,3 +1,5 @@
+use std::ops::RangeInclusive;
+
 /// Result of parsing a hunk header.
 ///
 /// Example hunk header: `@@ -1,2 +1,2 @@ Initial commit`
@@ -6,8 +8,8 @@
 /// making the line counts 2 for both.
 #[derive(Debug, PartialEq)]
 pub(crate) struct HunkHeader<'a> {
-    pub old_line_count: usize,
-    pub new_line_count: usize,
+    pub old_line_numbers: RangeInclusive<usize>,
+    pub new_line_numbers: RangeInclusive<usize>,
     pub title: Option<&'a str>,
 }
 
@@ -17,11 +19,6 @@ impl<'a> HunkHeader<'a> {
     /// Returns `None` if the line is not a valid hunk header.
     pub fn parse(line: &'a str) -> Option<Self> {
         let mut parts = line.splitn(5, ' ');
-        let mut header = Self {
-            old_line_count: 0,
-            new_line_count: 0,
-            title: None,
-        };
 
         if parts.next()? != "@@" {
             return None;
@@ -40,26 +37,32 @@ impl<'a> HunkHeader<'a> {
         let title_part = parts.next();
 
         // Parse the old line count
-        let old_line_counts = old_line_counts_part
+        let old_line_numbers = old_line_counts_part
             .trim_start_matches('-')
             .split(',')
             .collect::<Vec<_>>();
-        if old_line_counts.len() != 2 {
+        if old_line_numbers.len() != 2 {
             return None;
         }
-        header.old_line_count = 1 + old_line_counts[1].parse::<usize>().ok()?
-            - old_line_counts[0].parse::<usize>().ok()?;
+
+        let mut header = Self {
+            old_line_numbers: (0..=0),
+            new_line_numbers: (0..=0),
+            title: None,
+        };
+        header.old_line_numbers = old_line_numbers[1].parse::<usize>().ok()?
+            ..=old_line_numbers[0].parse::<usize>().ok()?;
 
         // Parse the new line count
-        let new_line_counts = new_line_counts_part
+        let new_line_numbers = new_line_counts_part
             .trim_start_matches('+')
             .split(',')
             .collect::<Vec<_>>();
-        if new_line_counts.len() != 2 {
+        if new_line_numbers.len() != 2 {
             return None;
         }
-        header.new_line_count = 1 + new_line_counts[1].parse::<usize>().ok()?
-            - new_line_counts[0].parse::<usize>().ok()?;
+        header.new_line_numbers = new_line_numbers[1].parse::<usize>().ok()?
+            ..=new_line_numbers[0].parse::<usize>().ok()?;
 
         // Grab the title if there is one
         header.title = title_part;
@@ -82,8 +85,8 @@ mod tests {
     fn test_simple_hunk_header() {
         assert_eq!(
             Some(HunkHeader {
-                old_line_count: 2,
-                new_line_count: 2,
+                old_line_numbers: (1..=2),
+                new_line_numbers: (1..=2),
                 title: None,
             }),
             HunkHeader::parse("@@ -1,2 +1,2 @@")
@@ -94,8 +97,8 @@ mod tests {
     fn test_hunk_header_with_title() {
         assert_eq!(
             Some(HunkHeader {
-                old_line_count: 2,
-                new_line_count: 2,
+                old_line_numbers: (1..=2),
+                new_line_numbers: (1..=2),
                 title: Some("Hello there"),
             }),
             HunkHeader::parse("@@ -1,2 +1,2 @@ Hello there")
@@ -106,8 +109,8 @@ mod tests {
     fn test_hunk_header_with_spaced_title() {
         assert_eq!(
             Some(HunkHeader {
-                old_line_count: 2,
-                new_line_count: 2,
+                old_line_numbers: (1..=2),
+                new_line_numbers: (1..=2),
                 title: Some("Hello  there"),
             }),
             HunkHeader::parse("@@ -1,2 +1,2 @@ Hello  there")
