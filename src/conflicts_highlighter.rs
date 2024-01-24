f2dd6a998e0fe6f3526b39e807fd9335af663de5
Johan Walles <johan.walles@gmail.com>
2024-01-24 13:55:39 +0100

    Highlight diff3 conflict sections

--- src/conflicts_highlighter.rs
+++ src/conflicts_highlighter.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use threadpool::ThreadPool;
 
 use crate::lines_highlighter::{LineAcceptance, LinesHighlighter, Response};
@@ -165,25 +167,64 @@ impl ConflictsHighlighter {
         );
     }
 
-    fn render_diff3(&self, _thread_pool: &ThreadPool) -> StringFuture {
-        // FIXME: Highlight self.c1, self.base and self.c2 before rendering
+    /// In this case we'll render:
+    /// * In section C1, we highlight additions compared to base
+    /// * In section base, we highlight things that have been removed either
+    ///   vs C1 or vs C2.
+    /// * In section C2, we highlight additions compared to base
+    fn render_diff3(&self, thread_pool: &ThreadPool) -> StringFuture {
+        let c1_header = self.c1_header.clone();
+        let c1 = self.c1.clone();
+        let base_header = self.base_header.clone();
+        let base = self.base.clone().unwrap();
+        let c2_header = self.c2_header.clone();
+        let c2 = self.c2.clone();
+        let footer = self.footer.clone();
 
-        let mut rendered = String::new();
-        rendered.push_str(&self.c1_header);
-        rendered.push('\n');
-        rendered.push_str(&self.c1);
+        return StringFuture::from_function(
+            move || {
+                let (base_vs_c1_tokens, c1_tokens, _, _) =
+                    refiner::to_highlighted_tokens(&base, &c1);
+                let highlighted_c1 = token_collector::render(&LINE_STYLE_CONFLICT_C2, c1_tokens);
 
-        rendered.push_str(&self.base_header);
-        rendered.push('\n');
-        rendered.push_str(self.base.as_ref().unwrap());
+                let (base_vs_c2_tokens, c2_tokens, _, _) =
+                    refiner::to_highlighted_tokens(&base, &c2);
+                let highlighted_c2 = token_collector::render(&LINE_STYLE_CONFLICT_C2, c2_tokens);
 
-        rendered.push_str(&self.c2_header);
-        rendered.push('\n');
-        rendered.push_str(&self.c2);
+                assert_eq!(base_vs_c1_tokens.len(), base_vs_c2_tokens.len());
 
-        rendered.push_str(&self.footer);
-        rendered.push('\n');
+                // Now, highlight everything in base that was removed either vs c1 or vs c2
+                let mut base_tokens = vec![];
+                for (base_vs_c1, base_vs_c2) in base_vs_c1_tokens.iter().zip(base_vs_c2_tokens) {
+                    if base_vs_c1.style as u8 >= base_vs_c2.style as u8 {
+                        base_tokens.push(base_vs_c1.clone());
+                    } else {
+                        base_tokens.push(base_vs_c2.clone());
+                    }
+                }
 
-        return StringFuture::from_string(rendered);
+                let highlighted_base =
+                    token_collector::render(&LINE_STYLE_CONFLICT_C1, base_tokens);
+
+                let mut rendered = String::new();
+                rendered.push_str(&c1_header);
+                rendered.push('\n');
+                rendered.push_str(&highlighted_c1);
+
+                rendered.push_str(&base_header);
+                rendered.push('\n');
+                rendered.push_str(&highlighted_base);
+
+                rendered.push_str(&c2_header);
+                rendered.push('\n');
+                rendered.push_str(&highlighted_c2);
+
+                rendered.push_str(&footer);
+                rendered.push('\n');
+
+                rendered
+            },
+            thread_pool,
+        );
     }
 }
