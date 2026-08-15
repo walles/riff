a0badca42aad64a2a9bd7e0731468fbbc472a9f4
Johan Walles <johan.walles@gmail.com>
2026-08-15 23:26:02 +0200

    Bump SDK setting

--- release.sh
+++ release.sh
@@ -15,7 +15,7 @@
 set -eu -o pipefail
 
 # List available SDKs using "xcodebuild -showsdks"
-CROSSBUILD_MACOS_SDK="macosx26.1"
+CROSSBUILD_MACOS_SDK="macosx26.2"
 
 # Git hooks can use this variable to avoid duplicating the CI work we do in here
 # anyway.
